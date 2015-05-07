66f539c56acb192e305ec826936c634a33fdfb8b
Ted Hooban <tedhooban@Teds-MacBook-Pro.local>
2015-05-07 15:24:43 -0400

    Initial Commit

---
+++ LeashTimeClientPet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeashTimeClientPet
+//
+//  Created by Ted Hooban on 5/7/15.
+//  Copyright (c) 2015 Ted Hooban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
